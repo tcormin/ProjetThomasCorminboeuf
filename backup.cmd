e505a0a025c179a164bce1e2d2ff66e97a9706c9
Corminboeuf Thomas <tcormin@bluewin.ch>
2013-05-18 12:05:49 +0200

    Etat des lieux correct pour Steve

--- backup.cmd
+++ backup.cmd
@@ -1,4 +1,4 @@
 call xcopy "C:\Dev\project" "C:\Users\Thomas\Dropbox\PBachelor\backup" /s /i /Y
-call xcopy "C:\Dev\project" "C:\Users\Thomas\Documents\GitHub\benevolat" /s /i /Y
+call xcopy "C:\Dev\project\benevolat" "C:\Users\Thomas\Documents\GitHub\ProjetThomasCorminboeuf" /s /i /Y
 
 pause
